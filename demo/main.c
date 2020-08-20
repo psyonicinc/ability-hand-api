91bb44935b8e714c5b1ac771e85ab609019fa257
ocanath_robotman <ocanath@gmail.com>
2020-08-20 23:09:55 +0100

    pressure tester outer done

--- demo/main.c
+++ demo/main.c
@@ -14,19 +14,18 @@ float current_time_sec(struct timeval * tv)
 3-pinky
 4-thumb
 */
-int get_idx_of_max_pressure(int ch, pres_union_fmt_i2c * pres_fmt)
+int get_idx_of_max_pressure(int finger, pres_union_fmt_i2c * pres_fmt)
 {
-	if(ch > 4)
-		ch = 4;
-	else if (ch < 0)
-		ch = 0;
-	int lowidx = ch*4;
-	int hidx = lowidx+4;
+	if(finger > 4)
+		finger = 4;
+	else if (finger < 0)
+		finger = 0;
+	int lowidx = finger*4;
 	uint16_t max = 0;
 	int idx_of_max = -1;
-	for(int i = lowidx; i < hidx; i++)
+	for(int i = lowidx; i < lowidx+4; i++)
 	{
-		uint16_t v = pres_fmt->v[ch];
+		uint16_t v = pres_fmt->v[i];
 		if(max < v)
 		{
 			idx_of_max = i;
@@ -43,9 +42,9 @@ void main()
 
 	/*Quick example of pre-programmed grip control (i.e. separate control mode from torque, velocity and position control)*/
 	set_grip(GENERAL_OPEN_CMD,100);
-	usleep(2000000);
+	usleep(500000);
 	set_grip(GENERAL_OPEN_CMD,100);
-	usleep(2000000);
+	usleep(500000);
 
 	//set_mode(DISABLE_PRESSURE_FILTER);	//uncomment for RAW pressure
 	//set_mode(DISABLE_TORQUE_VELOCITY_SAFETY);	//uncomment for UNSAFE torque and velocity control modes
@@ -65,10 +64,18 @@ void main()
 
 	float start_ts = current_time_sec(&tv);
 	float max_of_max[5] = {0};
+	
+	float reset_ts = 0;
 	while(1)
 	{
-		float t = fmod(current_time_sec(&tv) - start_ts, 10);
-
+		float t = current_time_sec(&tv) - start_ts;
+		
+		if(t > reset_ts)
+		{
+			for(int finger =0; finger<5; finger++)
+				max_of_max[finger] = 0.f;
+			reset_ts = t + 5;
+		}
 		/*
 		Pressure Indices:
 		Index: 	0-3
@@ -93,9 +100,9 @@ void main()
 		const char * name[5] = {"index","middle","ring","pinky","thumb"};
 		int finger = 0;
 		for(finger = 0; finger < 4; finger++)
-			printf("%s: %.3f, ", name[finger], max_of_max[finger]);
-		printf("%s: %.3f\r\n", name[finger], max_of_max[finger]);
-
+			printf("%s: %.4f, ", name[finger], max_of_max[finger]);
+		printf("%s: %.4f  ", name[finger], max_of_max[finger]);
+		printf("Countdown t minus: %.3f\r\n", reset_ts - t);
 			
 		for(int ch =0; ch < NUM_CHANNELS; ch++)
 			i2c_out.v[ch] = 0.f;
