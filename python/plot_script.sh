b43e44ef527d35763500bae7de45d445775ed93b
ocanath <ocanath@gmail.com>
2023-12-20 14:59:48 -0800

    scripts with usage printed and recommended settings

---
+++ python/plot_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "\r\n------------------------------------------------------------------------"
+echo "Run live_plot.py with recommended settings"
+echo "For this script to work properly, you must send the following commands to the Ability Hand over BLE"
+echo "using the developer console in the Ability Hand app, or a 3rd party GATT/BLE application such as nRF toolbox:"
+echo "Enable UART:                          We16"
+echo "If using RS485, enable RS485:         We35"
+echo "Enable byte stuffing (TX):            We46"
+echo "Enable byte unstuffing (RX):          We47"
+echo "\r\n\r\n------------------------------------------------------------------------"
+echo "Note: it is recommended to increase the baud rate to 1000000 if using RS485"
+echo "Set baud rate to max: 		Wp001:2:0x12"
+echo "If you set baud to max, ensure you call live_plot.py with -b 1000000"
+
+#run script with recommended settings
+sudo python3 live_plot.py --position --stuff
+
+#other common options
+# sudo python3 live_plot.py --position --stuff -b 1000000	#max baud
+# sudo python3 live_plot.py --touch --stuff 				#plot FSR data instead of position
