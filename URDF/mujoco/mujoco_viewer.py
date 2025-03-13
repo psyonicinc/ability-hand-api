8c0c185851cc7eb84e54ebdce610b2fb9fcf29f4
Justin <jkf@ualberta.ca>
2025-03-13 09:49:17 -0700

    more mujo

--- URDF/mujoco/mujoco_viewer.py
+++ URDF/mujoco/mujoco_viewer.py
@@ -11,19 +11,31 @@ model = mujoco.MjModel.from_xml_path(sys.argv[1])
 data = mujoco.MjData(model)
 
 # Actuators
-actuators = ['index_mcp_actuator', '_index_pip_actuator', 'middle_mcp_actuator',
-            '_middle_pip_actuator', 'ring_mcp_actuator', '_ring_pip_actuator',
-            'pinky_mcp_actuator', '_pinky_pip_actuator', 'thumb_flexor_actuato',
-            'thumb_rotator_actuator']
+actuators = [
+    "index_mcp_actuator",
+    "_index_pip_actuator",
+    "middle_mcp_actuator",
+    "_middle_pip_actuator",
+    "ring_mcp_actuator",
+    "_ring_pip_actuator",
+    "pinky_mcp_actuator",
+    "_pinky_pip_actuator",
+    "thumb_flexor_actuator",
+    "thumb_rotator_actuator",
+]
 
 # Create a viewer
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
         # Mimic Joints
-        act_ids = [mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, i) for i in actuators]
-        for j in range(0,7,2):
-            if act_ids[j] != -1 and act_ids[j+1] != -1:
-                data.ctrl[j+1] = data.ctrl[j] * 1.05851325 + 0.72349796
+        act_ids = [
+            mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, i) for i in actuators
+        ]
+        for j in range(0, 7, 2):
+            if act_ids[j] != -1 and act_ids[j + 1] != -1:
+                data.ctrl[act_ids[j + 1]] = (
+                    data.ctrl[act_ids[j]] * 1.05851325 + 0.72349796
+                )
 
         mujoco.mj_step(model, data)
         viewer.sync()
