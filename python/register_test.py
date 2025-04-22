7c285202686b97ead072cf7b45802334cc253283
ocanath <ocanath@gmail.com>
2025-04-22 09:09:00 -0700

    created flag write for enabling upsampling

--- python/register_test.py
+++ python/register_test.py
@@ -49,35 +49,35 @@ abh = AbilityHandSerialClient(baudrate=460800)
 abh.reply_mode = 2  # 1, 2, or 3
 abh.create_read_thread()
 
-index = [3000, 14, 1, 18, 1000, 14, 1, 1773, 4, 3546]
-middle = [3, 4, 1, 21, 0, 10, 1, 1773, 4, 3546]
-ring = [3, 4, 1, 21, 0, 10, 1, 1773, 4, 3546]
-pinky = [3000, 14, 1, 18, 1000, 14, 1, 1773, 4, 3546]
-thumbflexor = [3, 4, 1, 21, 0, 10, 1, 1773, 4, 3546]
-thumbrotator = [500, 0, 5, 3, 14, 6, 1, 1773, 8, 3546]
-gains = [index,middle,ring,pinky,thumbflexor,thumbrotator]
+# index = [3000, 14, 1, 18, 1000, 14, 1, 1773, 4, 3546]
+# middle = [3, 4, 1, 21, 0, 10, 1, 1773, 4, 3546]
+# ring = [3, 4, 1, 21, 0, 10, 1, 1773, 4, 3546]
+# pinky = [3000, 14, 1, 18, 1000, 14, 1, 1773, 4, 3546]
+# thumbflexor = [3, 4, 1, 21, 0, 10, 1, 1773, 4, 3546]
+# thumbrotator = [500, 0, 5, 3, 14, 6, 1, 1773, 8, 3546]
+# gains = [index,middle,ring,pinky,thumbflexor,thumbrotator]
 
-#read 
-size = 10   #number of elements per finger
-channels = [3, 5]
-for channel in channels:
-	for enumval in range(0,10):
-		addr = channel*size+enumval+1
+# #read 
+# size = 10   #number of elements per finger
+# channels = [3, 5]
+# for channel in channels:
+# 	for enumval in range(0,10):
+# 		addr = channel*size+enumval+1
 		
-		rv = read_uart_register(channel*size+enumval+1)
-		v = struct.unpack("<i", bytearray(rv[4:8]))[0]
-		print("Initial value:", v)
+# 		rv = read_uart_register(channel*size+enumval+1)
+# 		v = struct.unpack("<i", bytearray(rv[4:8]))[0]
+# 		print("Initial value:", v)
 
-		rc = write_uart_register(addr, gains[channel][enumval])
-		v = struct.unpack("<i", bytearray(rc[0:4]))[0]
-		if(v != 0x4e11fea4):
-			print("failed to write")
-		# else:
-		# 	print("Wrote addr", hex(addr), "success")
+# 		rc = write_uart_register(addr, gains[channel][enumval])
+# 		v = struct.unpack("<i", bytearray(rc[0:4]))[0]
+# 		if(v != 0x4e11fea4):
+# 			print("failed to write")
+# 		# else:
+# 		# 	print("Wrote addr", hex(addr), "success")
 
-		rv = read_uart_register(channel*size+enumval+1)
-		v = struct.unpack("<i", bytearray(rv[4:8]))[0]
-		print("Final value:", v)
+# 		rv = read_uart_register(channel*size+enumval+1)
+# 		v = struct.unpack("<i", bytearray(rv[4:8]))[0]
+# 		print("Final value:", v)
 
 
 #read the 64 bit unique id as two 32bit words from the hand over UART
@@ -86,6 +86,28 @@ rv = read_uart_register(61)
 v = struct.unpack("<i", bytearray(rv[4:8]))[0]
 print("reconstructed value:", hex(v))
 rv = read_uart_register(62)
+# print(bytearray_to_hex(rv))
+v = struct.unpack("<i", bytearray(rv[4:8]))[0]
+print("reconstructed value:", hex(v))
+
+
+#read the 64 bit unique id as two 32bit words from the hand over UART
+rv = read_uart_register(63)
+# print(bytearray_to_hex(rv))
+v = struct.unpack("<i", bytearray(rv[4:8]))[0]
+print("reconstructed value:", hex(v))
+
+
+
+rc = write_uart_register(63, 255)
+v = struct.unpack("<i", bytearray(rc[0:4]))[0]
+if(v != 0x4e11fea4):
+	print("failed to write")
+# else:
+# 	print("Wrote addr", hex(addr), "success")
+
+#read the 64 bit unique id as two 32bit words from the hand over UART
+rv = read_uart_register(63)
 # print(bytearray_to_hex(rv))
 v = struct.unpack("<i", bytearray(rv[4:8]))[0]
 print("reconstructed value:", hex(v))
