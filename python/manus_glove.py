b03a9ead40921fe7e2faa44d033401504e5bc32c
Justin <jkf@ualberta.ca>
2025-05-15 15:39:33 -0700

    all one plot

--- python/manus_glove.py
+++ python/manus_glove.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 import threading
+import math
 
+from scipy.spatial.transform import Rotation as R
 import rclpy
 from rclpy.node import Node
 
+from geometry_msgs.msg import Quaternion, TransformStamped
+from tf2_ros import TransformBroadcaster
+
 from manus_ros2_msgs.msg import ManusGlove  # Make sure this message is correctly built and sourced
 
-from plotting.plots import RealTimePlotTouch
+from plotting.plots import RealTimePlotTouch, RealTimePlotMotors, CombinedRealTimePlot
 from ah_wrapper.ah_serial_client import AHSerialClient
+from ah_simulators.ah_mujoco import AHMujocoSim
 
 def map_range(value, from_min, from_max, to_min, to_max):
     # Clamp value within from_min and from_max to prevent overshooting
@@ -23,6 +29,7 @@ class GloveSubscriber(Node):
 
     def __init__(self, client):
         super().__init__('glove_subscriber')
+        self.tf_broadcaster = TransformBroadcaster(self)
         self.subscription = self.create_subscription(
             ManusGlove,
             '/manus_glove_0',
@@ -30,10 +37,14 @@ class GloveSubscriber(Node):
             10  # QoS history depth
         )
         self.client = client
+        sim = AHMujocoSim(hand=self.client.hand, scene='ah_simulators/mujoco_xml/unitree_z1/scene.xml')
         self.mcps = [0,0,0,0,0,0]
         self.pips = [0,0,0,0,0,0]
         self.mcp_ranges = ((-1,80), (-2, 78), (-2,75), (-3,82), (-20,65), (-1,45))
         self.pip_ranges = ((0,90), (0,90), (0,90), (0,80), )
+
+        self.alternate = True
+
 
 
     def listener_callback(self, msg: ManusGlove):
@@ -51,8 +62,46 @@ class GloveSubscriber(Node):
                 self.mcps[2] = map_range(e.value, self.pip_ranges[2][0], self.pip_ranges[2][1], 0 , 100)
             elif e.type == "PinkyPIPStretch":
                 self.mcps[3] = map_range(e.value, self.pip_ranges[3][0], self.pip_ranges[3][1], 0 , 100)
-        
-        self.client.set_position(self.mcps)
+
+        if self.alternate:
+            self.client.set_position(self.mcps, reply_mode=0)
+            self.alternate = False
+        else:
+            self.client.set_position(self.mcps, reply_mode=1)
+            self.alternate = True
+
+
+        ori =  R.from_quat([msg.raw_sensor_orientation.x,
+                           msg.raw_sensor_orientation.y,
+                           msg.raw_sensor_orientation.z,
+                           msg.raw_sensor_orientation.w])
+
+        y_rot = R.from_euler('x', 90, degrees=True)
+
+        rotated = (y_rot * ori).as_quat()
+
+        # Publish orientation of glove
+        t = TransformStamped()
+
+        # Fill in header
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'world'
+        t.child_frame_id = 'glove'
+
+        # Use fixed translation
+        t.transform.translation.x = 0.0
+        t.transform.translation.y = 0.0
+        t.transform.translation.z = 1.0
+
+        # Use the received quaternion
+        t.transform.rotation.x = rotated[0]
+        t.transform.rotation.y = rotated[1]
+        t.transform.rotation.z = rotated[2]
+        t.transform.rotation.w = rotated[3]
+
+        # Send the transform
+        self.tf_broadcaster.sendTransform(t)
+
 
 def ros_loop(client):
     rclpy.init()
@@ -62,13 +111,12 @@ def ros_loop(client):
     rclpy.shutdown()
 
 
-
 def main(args=None):
     client = AHSerialClient()
     ros_t = threading.Thread(target=ros_loop, args=(client,))
     ros_t.start()
 
-    plotter = RealTimePlotTouch(client.hand)
+    plotter = CombinedRealTimePlot(client.hand)
     try:
         plotter.start()
     except KeyboardInterrupt:
